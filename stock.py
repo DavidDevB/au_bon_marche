875b8a46580431b00419559181d60ea71f8743a1
DavidB <david.bernard@fms-ea.com>
2025-10-22 13:51:21 +0200

    stock liste added to stock class

---
+++ stock.py
@@ -0,0 +1,29 @@
+from dataclasses import dataclass
+
+
+@dataclass
+class Stock:
+    stock = [
+    {"type": "fruit", "nom": "Clémentine", "stock": 6, "unité": "kg", "prix": 2.90},
+    {"type": "fruit", "nom": "Datte", "stock": 4, "unité": "kg", "prix": 7.00},
+    {"type": "fruit", "nom": "Grenade", "stock": 3, "unité": "kg", "prix": 3.50},
+    {"type": "fruit", "nom": "Kaki", "stock": 3, "unité": "kg", "prix": 4.50},
+    {"type": "fruit", "nom": "Kiwi", "stock": 5, "unité": "kg", "prix": 3.50},
+    {"type": "fruit", "nom": "Mandarine", "stock": 6, "unité": "kg", "prix": 2.80},
+    {"type": "fruit", "nom": "Orange", "stock": 8, "unité": "kg", "prix": 1.50},
+    {"type": "fruit", "nom": "Pamplemousse", "stock": 8, "unité": "pièce", "prix": 2.00},
+    {"type": "fruit", "nom": "Poire", "stock": 5, "unité": "kg", "prix": 2.50},
+    {"type": "fruit", "nom": "Pomme", "stock": 8, "unité": "kg", "prix": 1.50},
+
+    {"type": "légume", "nom": "Carotte", "stock": 7, "unité": "kg", "prix": 1.30},
+    {"type": "légume", "nom": "Choux de Bruxelles", "stock": 4, "unité": "kg", "prix": 4.00},
+    {"type": "légume", "nom": "Chou vert", "stock": 12, "unité": "pièce", "prix": 2.50},
+    {"type": "légume", "nom": "Courge butternut", "stock": 6, "unité": "pièce", "prix": 2.50},
+    {"type": "légume", "nom": "Endive", "stock": 5, "unité": "kg", "prix": 2.50},
+    {"type": "légume", "nom": "Épinard", "stock": 4, "unité": "kg", "prix": 2.60},
+    {"type": "légume", "nom": "Poireau", "stock": 5, "unité": "kg", "prix": 1.20},
+    {"type": "légume", "nom": "Potiron", "stock": 6, "unité": "pièce", "prix": 2.50},
+    {"type": "légume", "nom": "Radis noir", "stock": 10, "unité": "pièce", "prix": 5.00},
+    {"type": "légume", "nom": "Salsifis", "stock": 3, "unité": "kg", "prix": 2.50}
+]
+
