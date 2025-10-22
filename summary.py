d4fd24fb5a09ca8d8007fc4d37b5c8b7b2faf12a
DavidB <david.bernard@fms-ea.com>
2025-10-22 15:46:43 +0200

    docstring added to class Summary

--- summary.py
+++ summary.py
@@ -1,6 +1,18 @@
 
 class Summary:
-    def __init__(self, clients: list[list[str]], total: int, stock_left: list[dict]):
-        self.clients = clients
-        self.total = total
-        self.stock_left = stock_left
+
+    """
+    Classe Summary représentant le bilan de la journée. Peut retourner la liste des achats faits par tous les clients de la journée.
+    """
+
+    def __init__(self, summary: list[dict]):
+        self.summary = summary
+
+    summaries_by_client = []
+    for el in summary:
+        el[firstname] = el.firstname
+        el[lastname] = el.lastname
+        el[total] = el.total
+
+    def __repr__(self):
+        print("Summary of the day:")
