4808dc667ae2d70af31253b0067fc4a350c260cf
DavidB <david.bernard@fms-ea.com>
2025-10-22 15:41:19 +0200

    docstring added to class Vegetable

--- vegetable.py
+++ vegetable.py
@@ -3,6 +3,11 @@ from dataclasses import dataclass
 
 @dataclass
 class Vegetable:
+
+    """
+    Classe générique Vegetable pouvant retourner la valeur totale du légume restant en stock.
+    """
+
     name: str
     stock: int
     price: int
