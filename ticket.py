c7aa9ad592365f2a43516a37e32c278a10d666df
DavidB <david.bernard@fms-ea.com>
2025-10-22 15:45:38 +0200

    docstring added to class Ticket

--- ticket.py
+++ ticket.py
@@ -1,12 +1,18 @@
 
 
 class Ticket:
-    def __init__(self, content: list[dict]):
+
+    """
+    Classe Ticket représentant un ticket de caisse d'un client. Peut retourner la liste des achats et le total achetés par le client.
+    """
+
+    def __init__(self, client: dict, content: list[dict]):
+        self.client = client
         self.content = content
 
     def __repr__(self):
         total = 0
-        print(f"You bought :")
+        print(f"{self.client["firstname"]} {self.client["lastname"]}, you bought :")
         for el in self.content:
             total += el.price
             print(f"{el.quantity} {el.name}(s)")
