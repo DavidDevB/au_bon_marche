4674794665fe28dfb22cac4567c68282f6fb4f9e
DavidB <david.bernard@fms-ea.com>
2025-10-22 14:15:58 +0200

    staticmethod validate added to basket class and attribute client added to it

--- basket.py
+++ basket.py
@@ -1,5 +1,10 @@
 
 
 class Basket:
-    def __init__(self, content: list[dict]):
+    def __init__(self, client: list[str], content: list[dict]):
+        self.client = client
         self.content = content
+
+    @staticmethod
+    def validate():
+        return "validated"
