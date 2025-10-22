fbed0a839f825c4dfea5b3ccb23ebdba055a92f6
DavidB <david.bernard@fms-ea.com>
2025-10-22 15:42:32 +0200

    docstring added to class Fruit

--- fruit.py
+++ fruit.py
@@ -3,6 +3,11 @@ from dataclasses import dataclass
 
 @dataclass
 class Fruit:
+
+    """
+    Classe générique Fruit pouvant retourner la valeur totale d'un fruit restant en stock.
+    """
+
     name: str
     stock: int
     price: int
