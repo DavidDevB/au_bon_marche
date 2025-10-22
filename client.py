64bb21237f3b7ffe4281100881ea45b052b24bd7
DavidB <david.bernard@fms-ea.com>
2025-10-22 15:47:59 +0200

    docstring added to class Client

--- client.py
+++ client.py
@@ -1,9 +1,29 @@
 
 class Client:
 
-    def __init__(self, firstname, lastname, buys):
+    """
+    Classe générique Client pouvant retourner le prénom, le nom et/ou le total dépensé par le client pendant la journée.
+    """
+
+    def __init__(self, firstname, lastname, purchases):
         self.firstname = firstname
         self.lastname = lastname
-        self.buys = buys
+        self.purchases = purchases
+
+
+    def firstname(self) -> str:
+        return self.firstname
+
+
+    def lastname(self) -> str:
+        return self.lastname
+
+
+    @staticmethod
+    def total_spent() -> int:
+        total_spent = 0
+        for purchase in purchases:
+            total_spent += purchase["total"]
+        return total_spent
 
 
