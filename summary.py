00f7cd0dd27a05f4b27c7c5468cb7845dccf316b
Marie-Lorraine Collignon <marie-lorraine.collignon@fms-ea.com>
2025-10-22 15:17:03 +0200

    Cleaned branch after merge

--- summary.py
+++
@@ -1,6 +0,0 @@
-
-class Summary:
-    def __init__(self, clients: list[list[str]], total: int, stock_left: list[dict]):
-        self.clients = clients
-        self.total = total
-        self.stock_left = stock_left
