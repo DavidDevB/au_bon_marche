4dc192a8a07116cab35230ff0129c090d92f360d
DavidB <david.bernard@fms-ea.com>
2025-10-23 11:16:05 +0200

    get_clients_infos modified to return a joined string of firstname and lastname to make a unique id

--- au_bon_marche.py
+++ au_bon_marche.py
@@ -1,6 +1,7 @@
 
 from client import Client
 from stock import Stock
+from basket import Basket
 
 
 def client_or_owner() -> str | None:
@@ -18,7 +19,7 @@ def client_or_owner() -> str | None:
         return answer
 
 
-def get_client_infos() -> dict | None:
+def get_client_infos() -> str | None:
 
     """
     Retourne le prénom et le nom du client
@@ -34,7 +35,7 @@ def get_client_infos() -> dict | None:
         if lastname.isdigit() or None:
             print("Please enter a valid lastname.")
             continue
-        return {"firstname": firstname, "lastname": lastname}
+        return "".join[firstname, lastname]
 
 
 def choose_your_items():
@@ -60,31 +61,34 @@ def choose_your_items():
     items_by_name = {d["name"].lower(): d for d in stock}
 
     qty = None
-    while True:
-        name = input("Choose an item by name: ").strip().lower()
-        item = items_by_name.get(name)
-        if not item:
-            print("Choose an item from the stock.")
-            continue
-        break
-    print()
-    while True:
-        qty_str = input(f"Choose quantity (available={item['stock']} {item['unit']}): ").strip()
-        if not qty_str.isdigit():
-            print("Please enter an integer quantity.")
-            continue
-        qty = int(qty_str)
+    want_to_buy = True
+    while want_to_buy:
+        while True:
+            name = input("Choose an item by name: ").strip().lower()
+            item = items_by_name.get(name)
+            if not item:
+                print("Choose an item from the stock.")
+                continue
+            break
+        print()
+        while True:
+            qty_str = input(f"Choose quantity (available={item['stock']} {item['unit']}): ").strip()
+            if not qty_str.isdigit():
+                print("Please enter an integer quantity.")
+                continue
+            qty = int(qty_str)
 
-        if qty < 1:
-            print("Quantity must be at least 1.")
-            continue
+            if qty < 1:
+                print("Quantity must be at least 1.")
+                continue
 
-        if qty > item["stock"]:
-            print(f"Quantity must be available.")
-            continue
-        break
+            if qty > item["stock"]:
+                print(f"Quantity must be available.")
+                continue
+            Stock.decrease(item["name"], qty)
+            break
 
-    print(f"OK: {item['name']} x {qty} {item['unit']}")
-    return [item['name'],item['unit']]
+        print(f"You add: {item['name']} x {qty} {item['unit']} to your basket")
+
 
 
