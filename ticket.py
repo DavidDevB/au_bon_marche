1e40667530c260e5675a6c4a69c31922324781e5
DavidB <david.bernard@fms-ea.com>
2025-10-23 11:58:02 +0200

    add and total methods added to ticket class + split of client_id to get firstname and lastname added to ticket class

--- ticket.py
+++ ticket.py
@@ -1,4 +1,6 @@
 
+import re
+
 
 class Ticket:
 
@@ -6,13 +8,30 @@ class Ticket:
     Classe Ticket représentant un ticket de caisse d'un client. Peut retourner la liste des achats et le total achetés par le client.
     """
 
-    def __init__(self, client: dict, content: list[dict]):
-        self.client = client
+    def __init__(self, client_id: str, content: list[dict]):
+        self.client_id = client_id
         self.content = content
 
+
+    def add(self, name, quantity, price):
+        self.content.append(
+            {"name": name, "quantity": quantity, "price": price, "subtotal": round(price * quantity, 2)})
+
+
+    def total(self):
+        return round(sum(d["subtotal"] for d in self.content), 2)
+
+
     def __repr__(self):
+
+        def split_lower_upper(s: str):
+            return re.split(r'(?<=[a-z])(?=[A-Z])', s)
+
+        firstname = split_lower_upper(client_id)[0]
+        lastname = split_lower_upper(client_id)[1]
         total = 0
-        print(f"{self.client["firstname"]} {self.client["lastname"]}, you bought :")
+
+        print(f"{firstname} {lastname}, you bought :")
         for el in self.content:
             total += el.price
             print(f"{el.quantity} {el.name}(s)")
