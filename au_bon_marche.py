ce4c9e04270ecc7619332c2493232f15ce93da6d
DavidB <david.bernard@fms-ea.com>
2025-10-23 09:51:14 +0200

    function choose_your_items() created

--- au_bon_marche.py
+++ au_bon_marche.py
@@ -1,4 +1,7 @@
 
+from client import Client
+from stock import Stock
+
 
 def client_or_owner() -> str | None:
 
@@ -34,5 +37,54 @@ def get_client_infos() -> dict | None:
         return {"firstname": firstname, "lastname": lastname}
 
 
+def choose_your_items():
+    """
+    Demande au cliente de choisir un article et sa quantité
+    :return: list[str, float]
+    """
+
+    stock = Stock.stock
+
+    print("\n")
+    print("===FRUITS===")
+    for el in stock:
+        if el["type"] == "fruit":
+            print(f"{el['name']} | Price: {el['price']}€/kg | Stock: {el['stock']}")
+    print("\n")
+    print("===VEGETABLES===")
+    for el in stock:
+        if el["type"] == "vegetable":
+            print(f"{el['name']} | Price: {el['price']}€/kg | Stock: {el['stock']}")
+    print("\n")
+
+    items_by_name = {d["name"].lower(): d for d in stock}
+
+    qty = None
+    while True:
+        name = input("Choose an item by name: ").strip().lower()
+        item = items_by_name.get(name)
+        if not item:
+            print("Choose an item from the stock.")
+            continue
+        break
+    print()
+    while True:
+        qty_str = input(f"Choose quantity (available={item['stock']} {item['unit']}): ").strip()
+        if not qty_str.isdigit():
+            print("Please enter an integer quantity.")
+            continue
+        qty = int(qty_str)
+
+        if qty < 1:
+            print("Quantity must be at least 1.")
+            continue
+
+        if qty > item["stock"]:
+            print(f"Quantity must be available.")
+            continue
+        break
+
+    print(f"OK: {item['name']} x {qty} {item['unit']}")
+    return [item['name'],item['unit']]
 
 
